0aedb1c954999555a14671151b467c98bd88dceb
ikramersh <91307258+ikramersh@users.noreply.github.com>
2021-11-12 21:53:40 +1100

    lint

--- src/yahooFinExample.py
+++ src/yahooFinExample.py
@@ -1,4 +1,5 @@
-from yahoo_fin.stock_info import get_live_prices, get_live_price, tickers_nasdaq
+from yahoo_fin.stock_info import get_live_prices, get_live_price, 
+tickers_nasdaq
 
 
 stockList = ['CBA.AX', 'TLS.AX', 'amp.ax', 'ori.ax', 'bxb.ax']
