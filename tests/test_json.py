09d8970e9e71f38828e2b11b1d772b3831a058d6
ikramersh <91307258+ikramersh@users.noreply.github.com>
2021-11-12 21:19:56 +1100

    fix lint errors

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,10 +1,10 @@
 from src.jsonExample import jsonStr, jsonObjects
 
+
 def test_json():
-    myJsonStr = jsonStr({"names" : ("Ivan", "Helen", "Lara", "Ariella")})
+    myJsonStr = jsonStr({"names": ("Ivan", "Helen", "Lara", "Ariella")})
     jsonObjs = jsonObjects(myJsonStr)
 
     assert(jsonObjs['names'][0] == "Ivan")
     assert("Ariella" in jsonObjs['names'])
-    assert("John" not in jsonObjs['names'])
-    
+    assert("John" not in jsonObjs['names'])  
